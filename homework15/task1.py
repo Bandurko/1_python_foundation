e2221ad2d68fc4c9c497bad155ffd73aa16a3ef3
“Sergey” <s22-01-15@yandex.ru>
2024-03-18 00:45:34 +1000

    homework15

--- homework15/task1.py
+++ homework15/task1.py
@@ -44,32 +44,3 @@ if __name__ == '__main__':
     parser.add_argument('path', metavar='path for file', type=str, nargs='+', help='Enter path')
     args = parser.parse_args()
     print(get_file_info(*args.path))
-
-
-
-
-# import logging
-# import argparse
-
-
-# def abs_path(abs_path: str) -> tuple:
-#     result = []
-#     result.append(abs_path)
-#     for_result = abs_path.split('\\')
-#     result.append(for_result[-1])
-#     for_result2 = for_result[-1].split('.')
-#     result.append(for_result2[-1])
-#     logging.basicConfig(filename='path.log', filemode='a', encoding='utf-8', level=logging.DEBUG)
-#     logging.info(str(result))
-#     return tuple(result)
-#
-#
-# file_path = r"C:\\Users\Star\PycharmProjects\Education_Python\Homework\test.py"
-#
-# print(abs_path(file_path))
-
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser(description='Парсер для определения пути к файлу, файла и его расширения')
-#     parser.add_argument('path', metavar='path for file', type=str, nargs='+', help='Enter path')
-#     args = parser.parse_args()
-#     print(abs_path(*args.path))
