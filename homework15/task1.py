2924f395a20f47188b18bf3287361efda695c44c
“Sergey” <s22-01-15@yandex.ru>
2024-03-18 00:41:22 +1000

    homework15

---
+++ homework15/task1.py
@@ -0,0 +1,75 @@
+'''
+Возьмите любые 1-3 задания из прошлых домашних заданий. Добавьте к ним логирование ошибок и полезной информации. Также
+реализуйте возможность запуска из командной строки с передачей параметров.
+
+
+Возьмем первую задачу из ДЗ 5-го семинара
+
+Информация о файле
+
+Напишите функцию get_file_info, которая принимает на вход строку - абсолютный путь до файла.
+Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+Пример использования.
+На входе:
+
+file_path = "C:/Users/User/Documents/example.txt"
+
+На выходе:
+
+('C:/Users/User/Documents/', 'example', '.txt')
+'''
+
+
+import logging
+import argparse
+
+
+def get_file_info(file_path):
+    file_name = file_path.split("/")[-1]
+    file_extension = file_name.split(".")[-1]
+    path = file_path[:-len(file_name)]
+    result = (path, file_name[:-len(file_extension)-1], "." + file_extension)
+    logging.basicConfig(filename='path.log', filemode='a', encoding='utf-8', level=logging.DEBUG)
+    logging.info(str(result))
+    return (result)
+
+file_path = r"C:/Users/User/Documents/example.txt"
+
+print(get_file_info(file_path))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Парсер для определения пути к файлу, имени файла и его расширения')
+    parser.add_argument('path', metavar='path for file', type=str, nargs='+', help='Enter path')
+    args = parser.parse_args()
+    print(get_file_info(*args.path))
+
+
+
+
+# import logging
+# import argparse
+
+
+# def abs_path(abs_path: str) -> tuple:
+#     result = []
+#     result.append(abs_path)
+#     for_result = abs_path.split('\\')
+#     result.append(for_result[-1])
+#     for_result2 = for_result[-1].split('.')
+#     result.append(for_result2[-1])
+#     logging.basicConfig(filename='path.log', filemode='a', encoding='utf-8', level=logging.DEBUG)
+#     logging.info(str(result))
+#     return tuple(result)
+#
+#
+# file_path = r"C:\\Users\Star\PycharmProjects\Education_Python\Homework\test.py"
+#
+# print(abs_path(file_path))
+
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser(description='Парсер для определения пути к файлу, файла и его расширения')
+#     parser.add_argument('path', metavar='path for file', type=str, nargs='+', help='Enter path')
+#     args = parser.parse_args()
+#     print(abs_path(*args.path))
